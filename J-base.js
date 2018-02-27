86cdc099ca0e5c518f0728583fba664a6d79476d
jinnho <309126288@qq.com>
2018-02-27 11:06:38 +0800

    del J-touch

--- J-base.js
+++ J-base.js
@@ -1,9 +1,5 @@
 /**
- * Created by JinnHo on 2017/4/27.
- */
-
-/**
- * @param {String} src 专题路径(例如：红星美凯龙下的芝华仕沙发  redstar/cheers)
+ * @param {String} src 音频文件路径
  * @param {String} type1Url 默认音乐图标的地址
  * @param {String} type2Url 关闭音乐图标的地址
  * @param {boolean} isRotate 是否旋转
@@ -366,76 +362,3 @@ function gd_GetLocalAddress(callback) {
         });
     });
 }
-/**
- * 获取移动端touch事件方向与距离
- * @author JinnHo 309126288@qq.com
- * @version 1.0  2018/1/26
- * @param callback(data) 自定义回调函数，返回touchend结果
- * @param isOffset 自定义参数 是否获取相对位置（默认false）
- * @param moveFn 自定义参数 是否获取touchmove移动的位置（默认false）
- * @param swipeDis 自定义参数 滑动距离（默认100）
- * @constructor
- */
-(function ($) {
-    $.fn.extend({
-        J_swipe: function (callback,obj) {
-            var _extends = arguments[1] ? arguments[1] : [];
-            if (typeof _extends != "object") {
-                console.log("param error : extends param type(object) ->" + typeof _extends);
-                return;
-            }
-            var isOffset = _extends.offset == true ? 1 : 0;
-            var moveFn = _extends.fn ? _extends.fn : false;
-            var swipeDis = _extends.swipeDis ? _extends.swipeDis : 100;
-
-            var _this = this;
-            var offset = {x: _this.offset().left, y: _this.offset().top};
-            var touchesStart = {x: 0, y: 0};
-            var touchesMove = {x: 0, y: 0};
-            var touchesEnd = {x: 0, y: 0};
-            var moveDis = {x: 0, y: 0};
-
-            this.on("touchstart", function (e) {
-                touchesStart = {
-                    x: e.changedTouches[0].pageX - ((offset.x) * isOffset),
-                    y: e.changedTouches[0].pageY - ((offset.y) * isOffset)
-                };
-                offset = {x: _this.offset().left, y: _this.offset().top};
-            });
-            this.on("touchend", function (e) {
-                touchesEnd = {
-                    x: e.changedTouches[0].pageX - ((offset.x) * isOffset),
-                    y: e.changedTouches[0].pageY - ((offset.y) * isOffset)
-                };
-                var flag = "tap";
-                var dis = {x: touchesEnd.x - touchesStart.x, y: touchesEnd.y - touchesStart.y};
-                if (dis.x > swipeDis && Math.abs(dis.x) > Math.abs(dis.y)) {
-                    flag = "right";
-                } else if (dis.x < -swipeDis && Math.abs(dis.x) > Math.abs(dis.y)) {
-                    flag = "left";
-                } else if (dis.y > swipeDis && Math.abs(dis.y) > Math.abs(dis.x)) {
-                    flag = "down";
-                } else if (dis.y < -swipeDis && Math.abs(dis.y) > Math.abs(dis.x)) {
-                    flag = "up";
-                }
-                callback(flag,this);
-            });
-            this.on("touchmove", function (e) {
-                e.preventDefault();
-                touchesMove = {
-                    x: e.changedTouches[0].pageX - ((offset.x) * isOffset),
-                    y: e.changedTouches[0].pageY - ((offset.y) * isOffset)
-                };
-                if (typeof(moveFn) === 'function') {
-                    moveDis = {
-                        x: touchesMove.x - touchesStart.x,
-                        y: touchesMove.y - touchesStart.y,
-                        offx: offset.x,
-                        offy: offset.y
-                    };
-                    moveFn(moveDis, this);
-                }
-            });
-        }
-    });
-})(jQuery);
